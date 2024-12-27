ea6b458efb15fd3debc9ed729620740efcd2a816
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2024-12-27 21:33:12 +0530

    dashboard.html

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,18 @@ from django.shortcuts import render
 def home(request):
     return render (request, 'home.html')
 
+def login(request):
+    
+    #if method POST
+    #extract username
+    #extract password
+    
+    #autheticate user 
+    
+    #correct - dashboard.html
+    #else - login.html
+    
+    return render (request, 'login.html')
+
+def signup(request):
+    return render (request, 'signup.html')
